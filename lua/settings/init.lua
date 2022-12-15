42752a0b48f75f7627351d7ef22f9b1edd2bd12e
Thibault <thibault@babeau.fr>
2022-12-15 21:04:31 +0100

    Update init.lua

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,5 +1,6 @@
 local set = vim.opt
 
+-- General options
 set.expandtab = true
 set.smarttab = true
 set.shiftwidth = 4
@@ -9,11 +10,11 @@ set.hlsearch = true
 set.incsearch = true
 set.ignorecase = true
 set.smartcase = true
+
 set.splitbelow = true
 set.splitright = true
 set.wrap = flase
 set.scrolloff = 5
-
 set.fileencoding = 'utf-8'
 set.termguicolors = true
 set.syntax = "ON"
@@ -28,3 +29,17 @@ set.signcolumn = "yes"
 
 set.mouse = v
 set.mouse = a
+
+
+
+-- Keymaps
+let.mapleader = " "
+local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+map("n", "L", ":BufferLineCycleNext<CR>", opts)
+map("n", "H", ":BufferLineCyclePrev<CR>", opts)
+map("n", "<leader>x", ":bdelete<CR>", opts)
+map("n", "<leader>X", ":bdelete!<CR>", opts)
+map("n", "<leader>C", ":ClangFormatAutoToggle<CR>", opts)
