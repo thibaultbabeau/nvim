8daac31f03e1e74e02cb44b75167f1b6ed4ef238
root <thibault babeau>
2022-12-15 17:43:07 +0000

    first

---
+++ lua/packer-config/init.lua
@@ -0,0 +1,8 @@
+return require'packer'.startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'nvim-tree/nvim-web-devicons'
+    use 'EdenEast/nightfox.nvim'
+    use 'nvim-tree/nvim-tree.lua'
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+end)
